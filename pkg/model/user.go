e2f8e01ea09e5a7e7460d30e9738a2f46c011215
arashi87 <arasi27676271@gmail.com>
2022-05-13 22:40:00 +0800

    feat(user-model): add email column

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -3,5 +3,6 @@ package model
 type UserModel struct {
 	ID       uint   `gorm:"primaryKey"`
 	Name     string `gorm:"column:name;not null"`
+	Email    string `gorm:"column:email;not null"`
 	Password string `gorm:"column:password;not null"`
 }
