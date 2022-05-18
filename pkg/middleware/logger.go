89cf04c27c694060239fd0c963a432a8a2f4793d
arashi87 <arasi27676271@gmail.com>
2022-05-18 17:38:00 +0800

    feat(logger): add logger middleware to log request

---
+++ pkg/middleware/logger.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/arashi87/gin-template/pkg/common"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		startTime := time.Now()
+		ctx.Next()
+		common.Logger.WithFields(logrus.Fields{
+			"latency_time":   time.Since(startTime),
+			"request_method": ctx.Request.Method,
+			"request_uri":    ctx.Request.RequestURI,
+			"status_code":    ctx.Writer.Status(),
+			"client_ip":      ctx.ClientIP(),
+		}).Info()
+	}
+}
